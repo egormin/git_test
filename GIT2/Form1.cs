5a72c4a86e46f331e3003776871c4032ebc60b4a
poseidon20166 <poseidon20166@gmail.com>
2016-08-22 11:40:39 +0300

    second

--- GIT2/Form1.cs
+++ GIT2/Form1.cs
@@ -16,5 +16,10 @@ namespace GIT2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "test";
+        }
     }
 }
